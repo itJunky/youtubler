67a818e1e0bfeff5663eff29235d9ac4f548ec03
alp <a@aa.aa>
2023-11-14 18:15:04 +0300

    Добавлена дата публикации видео. Исправлен запуск без звука. Появился баг с одновременным запуском нескольких воспроизведений.

--- design.py
+++ design.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
-from PyQt5.QtCore import QCoreApplication, QRect, QMetaObject
+from PyQt5.QtCore import QCoreApplication, QRect, QMetaObject, Qt
 
 
 class MMainWindow(object):  # главное окно
@@ -16,24 +16,51 @@ class MMainWindow(object):  # главное окно
         self.menubar.setGeometry(QRect(0, 0, 785, 25))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
+
         self.thmb1 = QLabel('', self.centralwidget)
         self.thmb2 = QLabel('', self.centralwidget)
         self.thmb3 = QLabel('', self.centralwidget)
-        self.lbl1 = QLabel('', self.centralwidget)
-        self.lbl2 = QLabel('', self.centralwidget)
-        self.lbl3 = QLabel('', self.centralwidget)
+        self.title1 = QLabel('', self.centralwidget)
+        self.title2 = QLabel('', self.centralwidget)
+        self.title3 = QLabel('', self.centralwidget)
+        self.date1 = QLabel('11', self.centralwidget)
+        self.date2 = QLabel('11', self.centralwidget)
+        self.date3 = QLabel('11', self.centralwidget)
+
+        self.title1.setWordWrap(True)
+        self.title2.setWordWrap(True)
+        self.title3.setWordWrap(True)
+
+        self.title1.setSizePolicy(QSizePolicy.Expanding,
+                QSizePolicy.Expanding)
+        self.title1.setAlignment(Qt.AlignCenter)
+        #self.title1.setMinimumSize(100, 100)
+        #layout = QGridLayout(centralWidget)
+        #layout.addWidget(self.label)
+
         self.thmb1.move(10, 50)
         self.thmb2.move(10, 170)
         self.thmb3.move(10, 270)
-        self.lbl1.move(140, 50)
-        self.lbl2.move(140, 170)
-        self.lbl3.move(140, 270)
-        self.font = QFont()  # создаём объект шрифта
-        self.font.setFamily("Rubik")  # название шрифта
-        self.font.setPointSize(12)  # размер шрифта
-        self.font.setUnderline(False)  # подчёркивание
-        self.thmb1.setFont(self.font)  # задаём шрифт метке
-        #self.lbl.setGeometry(QRect(10, 10, 200, 15))
+        self.title1.move(140, 50)
+        self.title2.move(140, 170)
+        self.title3.move(140, 270)
+        self.date1.move(140, 120)
+        self.date2.move(140, 240)
+        self.date3.move(140, 340)
+        self.font_ttl = QFont()  # создаём объект шрифта
+        self.font_dt = QFont()
+        self.font_ttl.setFamily("DejaVuSerif")  # название шрифта
+        self.font_dt.setFamily("Rubik")
+        self.font_ttl.setPointSize(10)  # размер шрифта
+        self.font_dt.setPointSize(8)
+        self.font_ttl.setUnderline(False)  # подчёркивание
+        self.title1.setFont(self.font_ttl)  # задаём шрифт метке
+        self.title2.setFont(self.font_ttl)
+        self.title3.setFont(self.font_ttl)
+        self.date1.setFont(self.font_dt)
+        self.date2.setFont(self.font_dt)
+        self.date3.setFont(self.font_dt)
+        #self.title.setGeometry(QRect(10, 10, 200, 15))
         self.thmb1.adjustSize()
 
         self.lnkbtn1 = QPushButton('Смотреть', self.centralwidget)
@@ -42,15 +69,15 @@ class MMainWindow(object):  # главное окно
         self.lnkbtn1.resize(self.lnkbtn1.sizeHint())
         self.lnkbtn2.resize(self.lnkbtn2.sizeHint())
         self.lnkbtn3.resize(self.lnkbtn3.sizeHint())
-        self.lnkbtn1.move(450, 50)
-        self.lnkbtn2.move(450, 170)
-        self.lnkbtn3.move(450, 270)
+        self.lnkbtn1.move(520, 50)
+        self.lnkbtn2.move(520, 170)
+        self.lnkbtn3.move(520, 270)
         self.lnkbtn1.hide()
         self.lnkbtn2.hide()
         self.lnkbtn3.hide()
 
         self.lineEdit = QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QRect(10, 10, 291, 31))
+        self.lineEdit.setGeometry(QRect(10, 10, 620, 31))
         self.lineEdit.setText("Начни с поиска видео")
 
         self.closeButton = QPushButton('Close', self.centralwidget)
--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import urllib.request
-import subprocess
+from subprocess import Popen, PIPE, run
+import shlex
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPixmap
@@ -24,24 +25,35 @@ class WorkWin(QMainWindow):
         videos = ytb_get_videos_list(self.ui.lineEdit.text())
         print(videos)
         print(videos[0]['snippet']['thumbnails']['default']['url'])
+
         urllib.request.urlretrieve(videos[0]['snippet']['thumbnails']['default']['url'], 'default1.jpg')
         urllib.request.urlretrieve(videos[1]['snippet']['thumbnails']['default']['url'], 'default2.jpg')
         urllib.request.urlretrieve(videos[2]['snippet']['thumbnails']['default']['url'], 'default3.jpg')
+
         img1 = QPixmap('default1.jpg')
         img2 = QPixmap('default2.jpg')
         img3 = QPixmap('default3.jpg')
         self.ui.thmb1.setPixmap(img1)
         self.ui.thmb2.setPixmap(img2)
         self.ui.thmb3.setPixmap(img3)
-        self.ui.lbl1.setText(videos[0]['snippet']['title'])
-        self.ui.lbl2.setText(videos[1]['snippet']['title'])
-        self.ui.lbl3.setText(videos[2]['snippet']['title'])
+
+        self.ui.title1.setText(videos[0]['snippet']['title'])
+        self.ui.title2.setText(videos[1]['snippet']['title'])
+        self.ui.title3.setText(videos[2]['snippet']['title'])
+
+        self.ui.date1.setText(videos[0]['snippet']['publishedAt'])
+        self.ui.date2.setText(videos[1]['snippet']['publishedAt'])
+        self.ui.date3.setText(videos[2]['snippet']['publishedAt'])
+
+        self.ui.date1.adjustSize()
+        self.ui.date2.adjustSize()
+        self.ui.date3.adjustSize()
         self.ui.thmb1.adjustSize()
         self.ui.thmb2.adjustSize()
         self.ui.thmb3.adjustSize()
-        self.ui.lbl1.adjustSize()
-        self.ui.lbl2.adjustSize()
-        self.ui.lbl3.adjustSize()
+        self.ui.title1.adjustSize()
+        self.ui.title2.adjustSize()
+        self.ui.title3.adjustSize()
         self.ui.lnkbtn1.show()
         self.ui.lnkbtn2.show()
         self.ui.lnkbtn3.show()
@@ -53,8 +65,15 @@ class WorkWin(QMainWindow):
     def viewbtn(self, vid=None):
         vid = f'https://www.youtube.com/watch?v={vid}'
         print(f'vewbtn {vid}')
-        cmd = [f'ytdl --print-url "{vid}" | xargs mplayer -volume 80']
-        subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
+        dirtyurl = [f'ytdl -q 18 --print-url "{vid}"']
+
+        with Popen(dirtyurl, shell=True, stdout=PIPE) as process:
+            for line in process.stdout:
+                extractedurl = line.decode("utf-8")
+
+        print(f'Raw url: {extractedurl}')
+        cmd = shlex.split(f'mplayer -volume 80 "{extractedurl}"')
+        Popen(cmd, start_new_session=True)
 
 
 if __name__ == '__main__':
